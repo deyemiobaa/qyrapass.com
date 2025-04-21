bf6058afd6b1c492d2c16e5ff351f0505c62af38
Ramadan <Ramadanadex111@gmail.com>
2025-04-21 23:34:19 +0100

    added the poppins font

--- src/SectionEight.tsx
+++ src/SectionEight.tsx
@@ -6,7 +6,7 @@ export const SectionEight = () => {
          <div className="flex px-20 py-30 flex-col justify-center items-center bg-[#007BFF] space-y-4 rounded-2xl z-1000">
             <h1 className="text-2xl  md:text-4xl xl:text-[56px]  font-semibold tracking-tight text-white">Ready For Smarter Access ?</h1>
             <p className="text-sm text-center  md:text-sm xl:text-lg/[28px] text-white lg:max-w-lg">See how QyraPass works in 5 minutes.</p>
-            <button className="flex gap-2 text-[#007BFF] max-w-[160px] bg-white rounded-full px-5 py-3">
+            <button className="flex gap-2 text-[#007BFF] max-w-[180px] bg-white rounded-full px-5 py-3">
     <p>Get a Demo</p>
     <img src={arrow} alt="" className="text-white" />
   </button>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import arrow from "../assets/arrow.png"
 
 export const Button = () => {
   return (
-    <button className="flex gap-2 bg-[#007BFF] max-w-[160px] text-white rounded-full px-5 py-3">
+    <button className="flex gap-2 bg-[#007BFF] max-w-[180px] text-white rounded-full px-5 py-3">
     <p>Get a Demo</p>
     <img src={arrow} alt="" />
   </button>
